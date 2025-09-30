8aeaac8cb5bbf3036a06f5f12baa4e1ce9e39d2f
Praveen reddy Meruva <subbareddymeruva97@gmail.com>
2025-09-30 12:34:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   experimental: {
     esmExternals: 'loose'
   },
   webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
+    config.externals = [...config.externals, { canvas: "canvas" }];
     return config;
   },
 };
